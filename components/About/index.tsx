d4f830e86814cb6803d048a162cb9ae915145ed7
Kelvin Ochubili <kelvinochubili@gmail.com>
2024-06-15 12:53:08 +0100

    feat: added audiophile project

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,155 +1,156 @@
-import { hoverLink, hoverLinkOut } from "@/animations";
+import { hoverLink, hoverLinkOut } from '@/animations'
 
-import Image from "next/image";
+import Image from 'next/image'
 
-import c from "./About.module.scss";
+import c from './About.module.scss'
 
 const About = () => {
-  // const t = document.querySelector('[data-selector="about-cover"]');
-  // console.log(t);
+	// const t = document.querySelector('[data-selector="about-cover"]');
+	// console.log(t);
 
-  // useEffect(() => {
-  //   gsap.fromTo(
-  //     t?.querySelector("img"),
-  //     {
-  //       y: 85,
-  //     },
-  //     {
-  //       y: 0,
-  //       scrollTrigger: {
-  //         trigger: document.querySelector('[data-selector="about-cover"]'),
-  //         start: "top bottom",
-  //         // end: works.length - i === 1 ? "-=500" : "+=1000",
-  //         end: "top top",
-  //         scrub: true,
-  //         // markers: true,
-  //       },
-  //     }
-  //   );
-  // }, []);
+	// useEffect(() => {
+	//   gsap.fromTo(
+	//     t?.querySelector("img"),
+	//     {
+	//       y: 85,
+	//     },
+	//     {
+	//       y: 0,
+	//       scrollTrigger: {
+	//         trigger: document.querySelector('[data-selector="about-cover"]'),
+	//         start: "top bottom",
+	//         // end: works.length - i === 1 ? "-=500" : "+=1000",
+	//         end: "top top",
+	//         scrub: true,
+	//         // markers: true,
+	//       },
+	//     }
+	//   );
+	// }, []);
 
-  return (
-    <div className={c.about} data-selector="about">
-      <div className={c.about_hero} data-selector="about-cover">
-        {/* <div className={c.about_hero_cover}> */}
-        <Image
-          src="/bg-img-2.png"
-          alt="background image"
-          loading="eager"
-          width={968}
-          height={643}
-        />
-        {/* </div> */}
-        <h2>about:blank™</h2>
-        <h2>
-          about: <br /> blank™
-        </h2>
-      </div>
-      <div className={c.about_info}>
-        <div className={c.about_info_left}>
-          <p>
-            I am Kelvin Ochubili, A developer based in Lagos, Nigeria. I love
-            having fun with the web, creating things that are good for the eyes
-            to see, and to use.
-          </p>
-          <p>
-            I take great pride in my ability to blend aesthetics with
-            functionality, creating visually stunning websites and applications
-            that not only catch the eye but also provide a seamless user
-            experience. From conceptualizing the initial design to implementing
-            responsive layouts and optimizing performance, I meticulously work
-            through every stage of development to deliver exceptional results.
-          </p>
-        </div>
-        <div className={c.about_info_right}>
-          <div className={c.section}>
-            <h4>Experience</h4>
-            <ul className={c.exp}>
-              <li>
-                <span>Frontend Developer Intern</span>
-                <span>MAINSTACK, Jan &apos;22 - Apr &apos;22</span>
-              </li>
-              <li>
-                <span>Frontend Developer Intern</span>
-                <span>AYATRADES, Jun &apos;22 - Sep &apos;22</span>
-              </li>
-              <li>
-                <span>Freelance Frontend Developer</span>
-                <span>SUNBEAM STUDIO</span>
-              </li>
-            </ul>
-          </div>
-          <div className={c.section}>
-            <h4>Tools</h4>
-            <ul>
-              <li>HTML</li>
-              <li>SCSS</li>
-              <li>JAVASCRIPT</li>
-              <li>TYPESCRIPT</li>
-              <li>REACTJS</li>
-              <li>NEXTJS</li>
-              <li>GIT & GITHUB</li>
-              <li>GSAP, FRAMER MOTION.</li>
-            </ul>
-          </div>
-          <div className={c.section}>
-            <h4>Links</h4>
-            <ul>
-              <li>
-                <a
-                  href="https://github.com/iykekelvins"
-                  target="_blank"
-                  rel="noreferrer"
-                  onMouseOver={(e) => hoverLink(e)}
-                  onMouseOut={(e) => hoverLinkOut(e)}
-                >
-                  Github↗
-                  <div></div>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://twitter.com/iykekelvins"
-                  target="_blank"
-                  rel="noreferrer"
-                  onMouseOver={(e) => hoverLink(e)}
-                  onMouseOut={(e) => hoverLinkOut(e)}
-                >
-                  Twitter↗
-                  <div></div>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="mailto:kelvinochubili@gmail.com"
-                  target="_blank"
-                  rel="noreferrer"
-                  onMouseOver={(e) => hoverLink(e)}
-                  onMouseOut={(e) => hoverLinkOut(e)}
-                >
-                  Email↗
-                  <div></div>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className={c.section}>
-            <h4>Design</h4>
-            <a
-              href="https://twitter.com/olacodes"
-              target="_blank"
-              rel="noreferrer"
-              onMouseOver={(e) => hoverLink(e)}
-              onMouseOut={(e) => hoverLinkOut(e)}
-            >
-              Olacodes↗
-              <div></div>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className={c.about} data-selector='about'>
+			<div className={c.about_hero} data-selector='about-cover'>
+				{/* <div className={c.about_hero_cover}> */}
+				<Image
+					src='/bg-img-2.png'
+					alt='background image'
+					loading='eager'
+					width={968}
+					height={643}
+				/>
+				{/* </div> */}
+				<h2>about:blank™</h2>
+				<h2>
+					about: <br /> blank™
+				</h2>
+			</div>
+			<div className={c.about_info}>
+				<div className={c.about_info_left}>
+					<p>
+						I am Kelvin Ochubili, A developer based in Lagos, Nigeria. I love having fun
+						with the web, creating things that are good for the eyes to see, and to use.
+					</p>
+					<p>
+						I take great pride in my ability to blend aesthetics with functionality,
+						creating visually stunning websites and applications that not only catch the
+						eye but also provide a seamless user experience. From conceptualizing the
+						initial design to implementing responsive layouts and optimizing performance,
+						I meticulously work through every stage of development to deliver exceptional
+						results.
+					</p>
+				</div>
+				<div className={c.about_info_right}>
+					<div className={c.section}>
+						<h4>Experience</h4>
+						<ul className={c.exp}>
+							<li>
+								<span>Frontend Developer Intern</span>
+								<span>MAINSTACK, Jan &apos;22 - Apr &apos;22</span>
+							</li>
+							<li>
+								<span>Frontend Developer Intern</span>
+								<span>AYATRADES, Jun &apos;22 - Sep &apos;22</span>
+							</li>
+							<li>
+								<span>Freelance Frontend Developer</span>
+								<span>SUNBEAM STUDIO, Jan &apos;23 - Sep &apos;23</span>
+							</li>
+							<li>
+								<span>Frontend Developer</span>
+								<span>NativeBrands, Mar &apos;24 </span>
+							</li>
+						</ul>
+					</div>
+					<div className={c.section}>
+						<h4>Tools</h4>
+						<ul>
+							<li>HTML</li>
+							<li>CSS/SCSS</li>
+							<li>JAVASCRIPT</li>
+							<li>TYPESCRIPT</li>
+							<li>Tailwind CSS</li>
+							<li>REACTJS</li>
+							<li>NEXTJS</li>
+							<li>GIT & GITHUB</li>
+							<li>GSAP, FRAMER MOTION.</li>
+							<li>Redux Toolkit, RTQ Query.</li>
+						</ul>
+					</div>
+					<div className={c.section}>
+						<h4>Links</h4>
+						<ul>
+							<li>
+								<a
+									href='https://github.com/iykekelvins'
+									target='_blank'
+									rel='noreferrer'
+									onMouseOver={(e) => hoverLink(e)}
+									onMouseOut={(e) => hoverLinkOut(e)}>
+									Github↗
+									<div></div>
+								</a>
+							</li>
+							<li>
+								<a
+									href='https://twitter.com/iykekelvins'
+									target='_blank'
+									rel='noreferrer'
+									onMouseOver={(e) => hoverLink(e)}
+									onMouseOut={(e) => hoverLinkOut(e)}>
+									Twitter↗
+									<div></div>
+								</a>
+							</li>
+							<li>
+								<a
+									href='mailto:kelvinochubili@gmail.com'
+									target='_blank'
+									rel='noreferrer'
+									onMouseOver={(e) => hoverLink(e)}
+									onMouseOut={(e) => hoverLinkOut(e)}>
+									Email↗
+									<div></div>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div className={c.section}>
+						<h4>Design</h4>
+						<a
+							href='https://twitter.com/olacodes'
+							target='_blank'
+							rel='noreferrer'
+							onMouseOver={(e) => hoverLink(e)}
+							onMouseOut={(e) => hoverLinkOut(e)}>
+							Olacodes↗
+							<div></div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
 
-export default About;
+export default About
--- utils/index.ts
+++ utils/index.ts
@@ -1,59 +1,44 @@
 export const projects = [
-  {
-    title: "Bitpiler",
-    info: "A tech company that offers web services.",
-    url: "https://bitpiler.vercel.app",
-    type: "Creative Development",
-    imgs: [
-      "/bitpiler-1.png",
-      "/bitpiler-2.png",
-      "/bitpiler-3.png",
-      "/bitpiler-4.png",
-    ],
-  },
-  {
-    title: "Beverly Naya",
-    info: "A fansite for a Nigerian celebrity that speaks about her movies and her past achievements.",
-    url: "https://beverly-naya.vercel.app",
-    type: "Creative Development",
-    imgs: [
-      "/beverly-1.png",
-      "/beverly-2.png",
-      "/beverly-3.png",
-      "/beverly-4.png",
-    ],
-  },
-  {
-    title: "Maxwell",
-    info: "Portfolio development for Maxwell, A Frontend Developer.",
-    url: "https://maxwell-folio.vercel.app/",
-    type: "Web Exploration",
-    imgs: [
-      "/maxwell-1.png",
-      "/maxwell-2.png",
-      "/maxwell-3.png",
-      "/maxwell-4.png",
-    ],
-  },
+	{
+		title: 'Audiophile E-commerce',
+		info: 'An E-commerce website selling audio gadgets.',
+		url: 'https://audiophile-ecommerce-iykekelvins.vercel.app/',
+		type: 'Web Exploration',
+		imgs: [
+			'/audiophile-1.png',
+			'/audiophile-2.png',
+			'/audiophile-3.png',
+			'/audiophile-4.png',
+		],
+	},
+	{
+		title: 'Beverly Naya',
+		info: 'A fansite for a Nigerian celebrity that speaks about her movies and her past achievements.',
+		url: 'https://beverly-naya.vercel.app',
+		type: 'Creative Development',
+		imgs: ['/beverly-1.png', '/beverly-2.png', '/beverly-3.png', '/beverly-4.png'],
+	},
+	{
+		title: 'Maxwell',
+		info: 'Portfolio development for Maxwell, A Frontend Developer.',
+		url: 'https://maxwell-folio.vercel.app/',
+		type: 'Web Exploration',
+		imgs: ['/maxwell-1.png', '/maxwell-2.png', '/maxwell-3.png', '/maxwell-4.png'],
+	},
 
-  {
-    title: "Opay web",
-    info: "A Landing page redesign for  Nigerian fintech company, Opay.",
-    url: "https://opay-web.vercel.app/",
-    type: "Web Exploration",
-    imgs: ["/opay-1.png", "/opay-2.png", "/opay-3.png", "/opay-4.png"],
-  },
+	{
+		title: 'Opay web',
+		info: 'A Landing page redesign for  Nigerian fintech company, Opay.',
+		url: 'https://opay-web.vercel.app/',
+		type: 'Web Exploration',
+		imgs: ['/opay-1.png', '/opay-2.png', '/opay-3.png', '/opay-4.png'],
+	},
 
-  {
-    title: "HooBank",
-    info: "A Landing page design for fictional company, HooBank.",
-    url: "https://hoobank-iykekelvins.vercel.app/",
-    type: "Web Exploration",
-    imgs: [
-      "/hoobank-1.png",
-      "/hoobank-2.png",
-      "/hoobank-3.png",
-      "/hoobank-4.png",
-    ],
-  },
-];
+	{
+		title: 'HooBank',
+		info: 'A Landing page design for fictional company, HooBank.',
+		url: 'https://hoobank-iykekelvins.vercel.app/',
+		type: 'Web Exploration',
+		imgs: ['/hoobank-1.png', '/hoobank-2.png', '/hoobank-3.png', '/hoobank-4.png'],
+	},
+]
